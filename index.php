8c10cc311282dcd17e16ae593e05b490f2d9e8e9
kelvinsentosa <32952082+kelvinsentosa@users.noreply.github.com>
2019-04-04 21:56:04 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 	<form action="" method="POST">
 		Nama Kategori <input type="text" name="nama" required>
-		<input type="submit" name="submit" value="Submit" /> <input type="submit" name="load_data" value="Load Data" />
+		<input type="submit" name="submit" value="Submit" /> <input type="submit" name="ambi_data" value="Keluarkan Data" />
 	</form>
 	<?php
 		$host = "kelvinwebappserver.database.windows.net";
@@ -28,6 +28,26 @@
 				$stmt = $conn->prepare($sql_insert);
 				$stmt->bindValue(1,$nama);
 				$stmt->execute();
+			} catch (Exception $e) {
+				echo "Failed";
+			}
+		}elseif (isset($_POST['ambil_data'])) {
+			try {
+				$sql_select = "SELECT * FROM Kategori";
+				$stmt = $conn->query($sql_select);
+				$kategori = $stmt->fetchAll();
+				if (count($kategori) > 0) {
+					echo "<table border='1'>";
+					foreach ($kategori as $data) {
+						echo "<tr>";
+						echo "<td>".$data['id_kategori']."</td>";
+						echo "<td>".$data['nama_kategori']."</td>";
+						echo "</tr>";
+					}
+					echo "</table>";
+				}else{
+					echo "No Data Available";
+				}
 			} catch (Exception $e) {
 				echo "Failed";
 			}
